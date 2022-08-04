8bf2b9d78ebd4ed3f548848da12360be3fb90010
oprokopieva382 <oprokopieva382@gmail.com>
2022-08-04 16:23:50 -0500

    added axios

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+  let apiKey = "31062848022c12a9b5c54447286aa8b2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  return <h2>Hello from Weather</h2>;
+}
